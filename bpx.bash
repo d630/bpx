71be5cf4050128446b0988b0ee94751a8a31988c
D630 <d630@posteo.net>
2015-10-23 17:35:09 +0200

    rename variables

--- bpx.bash
+++ bpx.bash
@@ -12,27 +12,27 @@
 
 __bpx_precmd ()
 {
-        X_BPX_ERR=$?
+        BPX_ERR=$?
 
         builtin typeset f
 
-        for f in "${X_BPX_PRECMD_FUNC[@]}"
+        for f in "${BPX_PRECMD_FUNC[@]}"
         do
                 1>/dev/null builtin typeset -F "$f" && {
-                        ${f} "$X_BPX_ERR"
+                        ${f} "$BPX_ERR"
                 }
         done
 
         builtin wait
 
-        X_BPX_INTERACTIVE_MODE=1
+        BPX_INTERACTIVE_MODE=1
 }
 
 __bpx_preexec ()
 if
         builtin typeset c="$BASH_COMMAND"
         [[
-                $X_BPX_INTERACTIVE_MODE -eq 0 ||
+                $BPX_INTERACTIVE_MODE -eq 0 ||
                 $c =~ __bpx_pre(cmd|exec) ||
                 -n $COMP_LINE
         ]]
@@ -43,18 +43,18 @@ else
                 f \
                 h1;
         ((
-                X_BPX_INTERACTIVE_MODE =
+                BPX_INTERACTIVE_MODE =
                 BASH_SUBSHELL == 0
                 ? 1
                 : 0
         ))
         if
-                (( ${#X_BPX_PREEXEC_FUNC[@]} != 0 ))
+                (( ${#BPX_PREEXEC_FUNC[@]} != 0 ))
         then
                 builtin read -r _ h1 < <(
                         HISTTIMEFORMAT= builtin history 1
                 )
-                for f in "${X_BPX_PREEXEC_FUNC[@]}"
+                for f in "${BPX_PREEXEC_FUNC[@]}"
                 do
                         1>/dev/null builtin typeset -F "$f" && {
                                 ${f} "$c" "$h1"
@@ -66,27 +66,27 @@ fi
 
 __bpx_main ()
 if
-        [[ $PROMPT_COMMAND == __bpx_precmd || -n $X_BPX_ERR ]]
+        [[ $PROMPT_COMMAND == __bpx_precmd || -n $BPX_ERR ]]
 then
         builtin return 1
 else
         builtin unset -v \
-                X_BPX_ERR \
-                X_BPX_INTERACTIVE_MODE \
-                X_BPX_PRECMD_FUNC \
-                X_BPX_PREEXEC_FUNC \
-                X_BPX_PROMPT_COMMAND_OLD;
+                BPX_ERR \
+                BPX_INTERACTIVE_MODE \
+                BPX_PRECMD_FUNC \
+                BPX_PREEXEC_FUNC \
+                BPX_PROMPT_COMMAND_OLD;
         builtin typeset -gi \
-                X_BPX_ERR \
-                X_BPX_INTERACTIVE_MODE=1 \
-                X_BPX_USE_PREEXEC=$X_BPX_USE_PREEXEC;
-        builtin typeset -g +i X_BPX_PROMPT_COMMAND_OLD="$PROMPT_COMMAND"
+                BPX_ERR \
+                BPX_INTERACTIVE_MODE=1 \
+                BPX_USE_PREEXEC=$BPX_USE_PREEXEC;
+        builtin typeset -g +i BPX_PROMPT_COMMAND_OLD="$PROMPT_COMMAND"
         builtin unset -v PROMPT_COMMAND
         builtin typeset -g +i PROMPT_COMMAND=__bpx_precmd
         builtin typeset -g +i -a \
-                X_BPX_PRECMD_FUNC="()" \
-                X_BPX_PREEXEC_FUNC="()";
-        (( X_BPX_USE_PREEXEC )) && {
+                BPX_PRECMD_FUNC="()" \
+                BPX_PREEXEC_FUNC="()";
+        (( BPX_USE_PREEXEC )) && {
                 builtin shopt -u extdebug
                 builtin trap '__bpx_preexec' DEBUG
         }
